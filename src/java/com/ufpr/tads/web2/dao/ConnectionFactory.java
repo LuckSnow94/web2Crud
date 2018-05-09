e95cdf5bea96ec552761d414d4115e02546deed5
lka1994 <33166335+lka1994@users.noreply.github.com>
2018-05-09 14:54:17 -0300

    Update ConnectionFactory.java

--- src/java/com/ufpr/tads/web2/dao/ConnectionFactory.java
+++ src/java/com/ufpr/tads/web2/dao/ConnectionFactory.java
@@ -16,19 +16,23 @@ import java.util.logging.Logger;
  * @author luck
  */
 public class ConnectionFactory {
-    private final String dbDriver = "com.mysql.jdbc.Driver";
-    private final String dbUrl = "jdbc:mysql://localhost/web2";
+	//Mysql versão 8.0
+    private final String dbDriver = "com.mysql.cj.jdbc.Driver";
+    private final String dbUrl = "jdbc:mysql://localhost/web2?useSSL=false&useTimezone=true&serverTimezone=UTC";
     private final String dbUser = "root";
     private final String dbPwd = "luck";
             
     public Connection getConnection() throws InstantiationException, IllegalAccessException {
         try {
-            DriverManager.registerDriver((Driver) Class.forName(dbDriver).newInstance());
+            DriverManager.deregisterDriver((Driver) Class.forName(dbDriver).newInstance());
             return DriverManager.getConnection(dbUrl, dbUser, dbPwd);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+        	System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            throw new RuntimeException(ex);
+        } catch (ClassNotFoundException e) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, e);
         }
         return null;
     }    
